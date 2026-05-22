38be56366c6942268c98b1efee61d25fbbb8af49
Carlos <grimaldos882@gmail.com>
2026-05-22 10:42:38 +0100

    Database connection file

---
+++ config/bd.php
@@ -0,0 +1,20 @@
+<?php
+$host = "localhost";
+$dbname = "pcu_db";
+$username = "root";
+$password = "Carlos52842";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    die("Erro ao ligar à base de dados.");
+}
+?>
